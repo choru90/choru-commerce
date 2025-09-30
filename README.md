44725bd93f5e4adfec675e2eda1048b0eb6b5244
choru90 <jkc1619@gmail.com>
2025-09-30 17:25:34 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # choru-commerce
+# choru-commerce
---
+++ src/main/kotlin/com/qmit/commerce/ChoruCommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.qmit.commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChoruCommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChoruCommerceApplication>(*args)
+}
